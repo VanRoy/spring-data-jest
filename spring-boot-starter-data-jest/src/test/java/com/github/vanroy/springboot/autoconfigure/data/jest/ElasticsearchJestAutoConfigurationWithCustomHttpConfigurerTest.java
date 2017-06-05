ea61f8333ce9fe74fe9019d000276ecb3a03dc31
aminmc <aminmc@gmail.com>
2017-06-05 18:52:04 +0100

    Custom http configurer integration

---
+++ spring-boot-starter-data-jest/src/test/java/com/github/vanroy/springboot/autoconfigure/data/jest/ElasticsearchJestAutoConfigurationWithCustomHttpConfigurerTest.java
@@ -0,0 +1,57 @@
+package com.github.vanroy.springboot.autoconfigure.data.jest;
+
+
+import com.github.vanroy.springboot.autoconfigure.data.jest.repositories.ProductRepository;
+import io.searchbox.client.JestClient;
+import io.searchbox.client.http.JestHttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.Configurable;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.elasticsearch.jest.HttpClientConfigBuilderCustomizer;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ElasticsearchJestAutoConfigurationWithCustomHttpConfigurerTest.SpringBootStarterDataJestApplication.class)
+public class ElasticsearchJestAutoConfigurationWithCustomHttpConfigurerTest {
+
+    @Autowired
+    private JestClient jestClient;
+
+
+    @Test
+    public void should_jest_auto_configuration_have_custom_httpbuilder() {
+        RequestConfig config = ((Configurable) ((JestHttpClient) jestClient).getHttpClient()).getConfig();
+
+        int connectTimeout = config.getConnectTimeout();
+
+        assertThat(connectTimeout, is(3551));
+    }
+
+    @SpringBootApplication(exclude = {
+            ElasticsearchAutoConfiguration.class,
+            ElasticsearchDataAutoConfiguration.class,
+            ElasticsearchJestAWSAutoConfiguration.class
+    },
+            scanBasePackageClasses = ProductRepository.class)
+    public static class SpringBootStarterDataJestApplication {
+
+        @Bean
+        public HttpClientConfigBuilderCustomizer customizer() {
+            return httpBuilder ->
+                    httpBuilder
+                            .connTimeout(3551);
+
+        }
+
+    }
+}
