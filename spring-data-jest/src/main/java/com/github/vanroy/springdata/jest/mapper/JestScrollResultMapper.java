9679e499f6882bcfb27dbc388d2e592466c663d5
SHurth <shurth@thorne.com>
2017-11-02 20:03:31 -0500

    Changes to support passing Pageable downstream the same way Spring Data Elasticsearch does.

--- spring-data-jest/src/main/java/com/github/vanroy/springdata/jest/mapper/JestScrollResultMapper.java
+++ spring-data-jest/src/main/java/com/github/vanroy/springdata/jest/mapper/JestScrollResultMapper.java
@@ -1,7 +1,6 @@
 package com.github.vanroy.springdata.jest.mapper;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 import com.github.vanroy.springdata.jest.internal.SearchScrollResult;
 
